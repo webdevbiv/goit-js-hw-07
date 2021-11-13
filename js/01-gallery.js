6a55d43f11c9d7739a6727c2911fca045339db4d
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2021-11-13 22:18:34 +0200

    final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,58 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const refs = {
+    galleryRef: document.querySelector('.gallery')
+}
+console.log('test');
+console.log(refs.galleryRef);
+refs.galleryRef.addEventListener('click', selectItem)
+
+
+
+function selectItem(event) {
+    // console.dir(event.target)
+    // console.log(event.target);
+    if (event.target.nodeName !== 'IMG') {
+        console.log('not img');
+    }
+    event.preventDefault()
+    console.dir(event.target)
+    console.log(event.target.dataset.source)
+    const instance = basicLightbox.create(`
+        <img src="${event.target.dataset.source}"> width="800" height="600"
+    `)
+    instance.show(() => console.log('lightbox now visible'))
+    refs.galleryRef.addEventListener('keydown', function (event) {
+        if (event.key === 'Escape') {
+            instance.close()
+        }
+    })
+}
+
+
+function createGallery() {
+    const newGalleryItem = galleryItems.map(function (newItem) {
+        return `<div class="gallery__item">
+                    <a class="gallery__link" href="${newItem.original}">
+                        <img
+                            class="gallery__image"
+                            src="${newItem.preview}"
+                            data-source="${newItem.original}"
+                            alt="${newItem.description}"
+                        />
+                    </a>
+                </div> `
+    })
+    refs.galleryRef.insertAdjacentHTML('afterbegin', newGalleryItem.join(''))
+}
+
+
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `)
+
+// instance.show()
+
+createGallery()
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,47 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const refs = {
+    galleryRef: document.querySelector('.gallery')
+}
+
+createLightGallery()
+function createLightGallery() {
+    const gallery = document.createElement('div')
+    gallery.classList.add('gallery')
+    console.log(gallery);
+    refs.galleryRef.append(gallery)
+}
+
+console.dir(refs.galleryRef);
+const lightGalleryRef = refs.galleryRef.querySelector('.gallery')
+console.log(lightGalleryRef);
+
+createGalleryItems()
+function createGalleryItems() {
+    const newGalleryItem = galleryItems.map(function (newItem) {
+        return `<a class="gallery__item" href="${newItem.original}">
+                    <img class="gallery__image" src="${newItem.preview}" alt="${newItem.description}" />
+                </a>`
+    })
+    lightGalleryRef.insertAdjacentHTML('afterbegin', newGalleryItem.join(''))
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    overlay: true,
+    animationSpeed: 150,
+    captionSelector: 'img',
+    captionType: 'attr',
+    captionsData: 'alt',
+});
+
+// console.log(lightGalleryRef);
+// lightGalleryRef.addEventListener('click', clickHandler)
+
+// function clickHandler(event) {
+//     if (event.target.nodeName !== 'IMG') {
+//         console.log('not img');
+//     }
+//     event.preventDefault()
+//     var lightbox = $('.gallery a').simpleLightbox({ /* options */ });
+// }
